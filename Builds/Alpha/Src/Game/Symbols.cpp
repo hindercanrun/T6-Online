6844565a5d1b941922a0b7cfc5bafe4d61c5a388
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-07-16 17:55:27 +0800

    Alpha: clean up, should be good now

--- Builds/Alpha/Src/Game/Symbols.cpp
+++
@@ -1,8 +0,0 @@
-#include "../Std_Include.hpp"
-#include "Symbols.hpp"
-
-namespace Symbols
-{
-	// Functions
-	Com_Printf_t Com_Printf = Com_Printf_t(0x82519E38);
-}
--- Builds/Alpha/Src/Std_Include.hpp
+++ Builds/Alpha/Src/Std_Include.hpp
@@ -1,32 +1,11 @@
 #pragma once
 
-#define TITLE_ID 0x415608C3 // Black Ops II
-
-#include <string>
-#include <vector>
-#include <cstdint>
-#include <cwctype>
-#include <algorithm>
-#include <iostream>
-#include <ctime>
-#include <cstdio>
-#include <unordered_map>
-#include <fstream>
+#define TITLE_ID 0x415608C3 // Black Ops 2
 
 #include <xtl.h>
-#include <stdio.h> // for sprintf_s
 #include <stdint.h>
-#include <ppcintrinsics.h>
-#include <direct.h>
 
-#include <xtl.h>
-#include <xboxmath.h>
-
-#include "Game\Symbols.hpp"
-
-// xbox kernel stuff
 extern "C"
 {
 	NTSYSAPI DWORD NTAPI XamGetCurrentTitleId(VOID);
-	NTSYSAPI VOID NTAPI DbgPrint(const char* s, ...);
 }
